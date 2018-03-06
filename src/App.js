2ff39ede251bddf905261d59f8ec60e41853d46a
Vinay Ashok <vinay.ashok26@gmail.com>
2018-03-05 20:49:14 -0800

    Initial Setup

--- src/App.js
+++ src/App.js
@@ -1,39 +1,9 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-
-
-class ContactList extends React.Component {
-  render() {
-    const people = this.props.contacts
-
-    return <ol>
-      {people.map(person => (
-          <li key={person.name}>{person.name}</li>
-        ))}
-     </ol>
-
-  }
-}
-
+import React, { Component } from "react";
+import logo from "./logo.svg";
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <ContactList contacts={[
-          { name: "Vinay" }, 
-          { name: "Bhavana" }, 
-          { name: "Love" }
-        ]}/>
-        <ContactList contacts={[
-          { name: "Vinay" }, 
-          { name: "Bhavana" }
-        ]}/>
-        
-      </div>
-    );
+    <div>Hello World</div>
   }
 }
 
